6f033dc6fc87f74c28af3e3b0f756eb324fc8fa0
'bin3005' <'bin3005@naver.com'>
2020-12-05 14:12:31 +0900

    CodeUp100(1~20) with Java

---
+++ Prc1/src/Prac2.java
@@ -0,0 +1,98 @@
+import java.util.Scanner;
+
+
+public class Prac2 {
+
+	public static void main(String[] args) {
+		//2번 
+//		System.out.println("Hello World");
+		
+		//3번
+//		System.out.println("Hello");
+//		System.out.println("World");
+		
+		//4번
+//		System.out.println("'Hello'");
+		
+		//5번
+//		System.out.println("\"Hello World\"");
+		
+		
+		//6번
+//		System.out.println("\"@#$%^&*()\"");
+		
+		//7번
+//		System.out.println("\"C:\\Download\\hello.cpp\"");
+		
+		//8번
+//		System.out.println("\u250C\u252C\u2510\n\u251C\u253C\u2524\n\u2514\u2534\u2518");
+		
+		//9번
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.nextInt());
+		
+		//10번
+//		char c;
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.next());
+		
+		//11번
+//		float f;
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.nextFloat());
+
+		//12번
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.nextInt() + " " + in.nextInt());
+		
+		//13번
+//		char a, b;
+//		Scanner in = new Scanner(System.in);
+//		a= in.next().charAt(0); b=in.next().charAt(0);
+//		System.out.println(b + " " + a);
+		
+		//14번
+//		Scanner in = new Scanner(System.in);
+//		System.out.format("%.2f", in.nextDouble());
+		
+		//15번
+//		int a = 0 ; 
+//		Scanner in = new Scanner(System.in);
+//		a = in.nextInt();
+//		for(int i = 0 ; i < 3 ; i ++) {
+//			System.out.print(a+" ");
+//		}
+//
+		
+		//16번
+//		int hour, minute;
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.nextLine());
+		
+		//17번
+//		String full;
+//		Scanner in = new Scanner(System.in);
+//		full = in.nextLine();
+//		int year = Integer.parseInt(full.split("\\.")[0]);
+//		int month = Integer.parseInt(full.split("\\.")[1]);
+//		int day = Integer.parseInt(full.split("\\.")[2]);
+//		System.out.printf("%04d.%02d.%02d", year , month,day);
+		
+		//18번
+//		String rno;
+//		Scanner in = new Scanner(System.in);
+//		rno = in.nextLine();
+//		System.out.println(rno.split("-")[0] + rno.split("-")[1]);
+		
+		//19번
+//		Scanner in = new Scanner(System.in);
+//		System.out.println(in.nextLine());
+		
+		//20번
+		Scanner in = new Scanner(System.in);
+		System.out.println(in.nextLine());
+		
+		
+	}
+
+}
